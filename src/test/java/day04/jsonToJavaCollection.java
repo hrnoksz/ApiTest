c18444df83ea27d6589fd20f97ddae7771827918
hrnoksz <harun1973@yahoo.com>
2022-09-13 11:21:10 +0300

    jsonToJavaCollection.java allSpartansToMap done

--- src/test/java/day04/jsonToJavaCollection.java
+++ src/test/java/day04/jsonToJavaCollection.java
@@ -8,6 +8,8 @@ import utilities.ConfigurationReader;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -28,7 +30,7 @@ public class jsonToJavaCollection {
                 .when().get("api/spartans/{id}")
                 .then().statusCode(200).extract().response();
 
-        response.prettyPrint();
+        //response.prettyPrint();
 
         Map<String, Object> jsonMap = response.as(Map.class);
         System.out.println("jsonMap = " + jsonMap);
@@ -39,11 +41,29 @@ public class jsonToJavaCollection {
         String actualName = (String) jsonMap.get("name");
         String actualGender = (String) jsonMap.get("gender");
         long actualPhone = (long) jsonMap.get("phone");
-        System.out.println("actualPhone = " + actualPhone);
 
         assertThat(actualId, is(46));
         assertThat(actualName, is("Delora"));
         assertThat(actualGender, is("Female"));
         assertThat(actualPhone, is(4115324496l));
+    }
+
+    @Test
+    public void allSpartansToMap() {
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("api/spartans")
+                .then().statusCode(200).extract().response();
+
+        List<Map<String, Object>> jsonList = response.as(List.class);
+
+        System.out.println("jsonList = " + jsonList);
+
+        System.out.println("jsonList.get(0).get(\"name\") = " + jsonList.get(0).get("name"));
+
+        Map<String, Object> spartan3 = jsonList.get(2);
+        System.out.println("spartan3 = " + spartan3);
+
+        assertEquals(jsonList.get(1).get("name"), "Nels");
     }
 }
