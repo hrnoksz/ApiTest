42d895b0898d4f107b6e8a7c14f39a7f43548090
hrnoksz <harun1973@yahoo.com>
2022-09-21 13:50:54 +0300

    PutAndPatchRequest.java patchRequest and deleteRequest done

--- src/test/java/day06/PutPatchAndDeleteRequest.java
+++ src/test/java/day06/PutPatchAndDeleteRequest.java
@@ -4,9 +4,6 @@ import org.testng.annotations.BeforeClass;
 import day05_POJO.Spartan;
 
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
@@ -16,9 +13,8 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
-import static org.testng.Assert.*;
 
-public class PutAndPatchRequest {
+public class PutPatchAndDeleteRequest {
 
     @BeforeClass
     public void init(){
@@ -50,6 +46,51 @@ public class PutAndPatchRequest {
 
         assertThat(putRequestMap.get("phone"), is(spartanPosted.getPhone()));
 
+
+    }
+
+    @Test
+    public void patchRequest(){
+        //Just like post request we have different options to send body, we will go with map
+        Map<String, Object> putRequestMap = new HashMap<>();
+        putRequestMap.put("phone", 5554111333l);
+
+        given().contentType(ContentType.JSON)
+                .body(putRequestMap)
+                .and().pathParam("id", 295)
+                .when().patch("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+        //send a GET request after update, make sure updated field changed, or the new info matching
+        //with requestBody that we send
+
+        Spartan spartanPosted = given().accept(ContentType.JSON)
+                .and().pathParam("id", 295)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .extract().response().as(Spartan.class);
+
+        assertThat(putRequestMap.get("phone"), is(spartanPosted.getPhone()));
+
+    }
+
+    @Test
+    public void deleteSpartan(){
+
+        int idToDelete = 45;
+
+        given().pathParam("id", idToDelete)
+                .when().delete("/api/spartans/{id}")
+                .then().statusCode(204);
+        //Send a GET request after you delete make sure you are getting 404
+        try {
+            given().pathParam("id", idToDelete)
+                    .when().get("/api/spartans/{id}")
+                    .then().statusCode(404);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
