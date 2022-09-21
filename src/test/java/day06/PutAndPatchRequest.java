83df29d9737626f5e22d522786aa39f075b21401
hrnoksz <harun1973@yahoo.com>
2022-09-21 13:21:01 +0300

    PutAndPatchRequest.java putRequest done

--- src/test/java/day06/PutAndPatchRequest.java
+++ src/test/java/day06/PutAndPatchRequest.java
@@ -25,12 +25,32 @@ public class PutAndPatchRequest {
         baseURI = ConfigurationReader.getProperty("spartan_url");
     }
     @Test
-    public void PutRequest(){
+    public void putRequest(){
         //Just like post request we have different options to send body, we will go with map
         Map<String, Object> putRequestMap = new HashMap<>();
         putRequestMap.put("name", "Frank");
         putRequestMap.put("gender", "Male");
-        putRequestMap.put("phone", 5554111155l);
+        putRequestMap.put("phone", 5554111333l);
+
+        given().contentType(ContentType.JSON)
+                .body(putRequestMap)
+                .and().pathParam("id", 295)
+                .when().put("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+        //send a GET request after update, make sure updated field changed, or the new info matching
+        //with requestBody that we send
+
+        Spartan spartanPosted = given().accept(ContentType.JSON)
+                .and().pathParam("id", 295)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .extract().response().as(Spartan.class);
+
+        assertThat(putRequestMap.get("phone"), is(spartanPosted.getPhone()));
+
+
     }
 
 }
